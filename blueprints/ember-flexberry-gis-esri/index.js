247e4a1fb0e1075e8ec26e57e0eed001e87fff87
kuzkok <kuzkok@gmail.com>
2017-04-13 15:30:54 +0100

    Change bower packages to version specific

--- blueprints/ember-flexberry-gis-esri/index.js
+++ blueprints/ember-flexberry-gis-esri/index.js
@@ -2,6 +2,6 @@
 module.exports = {
 
   afterInstall: function (options) {
-    return this.addBowerPackagesToProject([{ name: 'esri-leaflet', source: 'https://github.com/Esri/esri-leaflet#^2.0.3' }]);
+    return this.addBowerPackagesToProject([{ name: 'esri-leaflet', target: '^2.0.3' }]);
   }
 };
--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 module.exports = {
   name: 'ember-flexberry-gis-esri',
 
+  isDevelopingAddon() {
+    return true;
+  },
+
   included: function (app) {
     this._super.included.apply(this._super, arguments);
 
