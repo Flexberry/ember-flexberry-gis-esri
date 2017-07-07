0beafdb1e6debc46a58a2efcb1d5c0a30bb91e5f
kuzkok <kuzkok@gmail.com>
2017-07-07 14:08:20 +0100

    Remove isDevelopingAddon from index, bump version

--- index.js
+++ index.js
@@ -4,10 +4,6 @@
 module.exports = {
   name: 'ember-flexberry-gis-esri',
 
-  isDevelopingAddon() {
-    return true;
-  },
-
   included: function (app) {
     this._super.included.apply(this._super, arguments);
 
